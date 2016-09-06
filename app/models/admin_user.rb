ab73061552bf6baf73871d5a456fa79d795b4407
bhushan-amura <bhushan.a@amuratech.com>
2016-09-06 19:17:45 +0530

    Update models admin user, page, section, subject

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -1,4 +1,10 @@
 class AdminUser < ActiveRecord::Base
-  # To configure the table name
+
+  # To configure a different table name:
   # self.table_name = "admin_users"
+
+  has_and_belongs_to_many :pages
+  has_many :section_edits
+  has_many :sections, :through => :section_edits
+
 end
